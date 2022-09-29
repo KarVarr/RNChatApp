d9254acc9910637d900821c29ca267389115ddbf
Karen Vardanian <karenvardanian@MacBook-Air-Karen.local>
2022-09-29 18:42:31 +0300

    started the project

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import LoginScreen from './screens/LoginScreen';
+
+
+
 
 export default function App() {
   return (
